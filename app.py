c7a53e3b123c0e39a3224b73b41e1d56158052f3
Duc Luu <trungducmic@gmail.com>
2023-11-02 14:05:37 -0700

    removed audio file

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ def main():
 
     if st.button('Analyze'):
         audio_input = YouTube(audio_input).streams.filter(only_audio=True).first()
-        audio_input = audio_input.download()
+        audio_input = audio_input.download(type='mp3')
+        print(type(audio_input))
         st.audio(audio_input)
+        
         y, sr = librosa.load(audio_input)
         y_harmonic, y_percussive = librosa.effects.hpss(y)
         unebarque_fsharp_min = Tonal_Fragment(y_harmonic, sr, tend=22)
